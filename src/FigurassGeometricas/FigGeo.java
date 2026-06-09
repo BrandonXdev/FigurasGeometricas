43729ea36bfa7bce7e97df405ef5bd3cdd9263a2
BrandonXdev <brchevezac@est.utn.ac.cr>
2026-06-09 16:40:52 -0600

    Clase FigGeo
    Se crea la clase abstracta FigGeo con los atributos area y perimetro que existen en todas las figuras geometricas

---
+++ src/FigurassGeometricas/FigGeo.java
@@ -0,0 +1,21 @@
+
+package FigurassGeometricas;
+
+
+public abstract class FigGeo {
+    
+    protected double perimetro; // se ponen protegidos para que la clase hija los puedan usar 
+    protected double area;
+
+    public double getPerimetro() {
+        return perimetro;
+    }
+
+    public double getArea() {
+        return area;
+    }  
+    
+   public abstract double calcularPerimetro();
+   public abstract double calcularArea();
+       
+}
